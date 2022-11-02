ebb51c96a5c0b1e4af6807f708fab742e4426e04
Mstfyaman <forfreedom067@gmail.com>
2022-11-02 15:27:13 +0300

    new Calismam . 01.11.2022 dahil

---
+++ src/test/java/com/amazon/test/day4_Xpath/AbsoluteXpath.java
@@ -0,0 +1,24 @@
+package com.amazon.test.day4_Xpath;
+
+import com.amazon.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class AbsoluteXpath {
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+
+        driver.get("https://www.krafttechexlab.com");
+         //driver.manage().window().maximize();
+
+        String accualText = driver.findElement(By.xpath("/html/body/main/div/h1")).getText();
+         // burda child classtan parrent classla doğru yazdık.  bunu kısayol olarak siteden copy=full xpath ile alabiliriz.
+         System.out.println("accualText = " + accualText);
+
+
+         // ORNEK :   /html/body/div/form/div[2]/input
+
+
+    }
+}
---
+++ src/test/java/com/amazon/test/day4_Xpath/RelativeXpath.java
@@ -0,0 +1,23 @@
+package com.amazon.test.day4_Xpath;
+
+import com.amazon.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class RelativeXpath {
+    public static void main(String[] args) {
+
+        WebDriver driver= WebDriverFactory.getDriver("chrome");
+        driver.get("https://www.hepsiburada.com/");
+
+        String text=driver.findElement(By.xpath("(//a[@href='https://www.hepsiburada.com/cozummerkezi'])[1]")).getText();
+            // normalde : "//a[@href='https://www.hepsiburada.com/cozummerkezi']" böyle. dış parantezler ve [1] yok.
+                    // böyle yaparak; mesela 10 tane bu texten var biz [1]. ciyi istemiş oluyoruz.
+        System.out.println("text = " + text);
+
+        // Relative xpath cift slah (//) ile baslar ve sayfanin herhangi bir yerine
+        //bakmayi ifade eder
+
+
+    }
+}
